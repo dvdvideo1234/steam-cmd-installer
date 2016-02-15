3a5724b4908968774409b3e6875fcf5d252c3ec0
Gousaid67 <erik.lg@student.intsch.se>
2016-02-15 17:57:55 +0100

    added anonymous logging support

---
+++ steamcmdlazyinstaller2.0.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+echo thx for using this script,its my first script so make any suggestion to the github page
+echo this will instal the required dependencies
+sudo apt-get install lib32gcc1
+echo making directory /steamcmd at /home/$USER/ ....
+mkdir ~/steamcmd
+echo switching to the created folder
+cd ~/steamcmd
+echo Downloading steamcmd
+wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+echo Extracting
+tar -xvzf steamcmd_linux.tar.gz
+echo input your username for steam
+read -r a
+if test "$a" = "anonymous"
+then
+     echo input the appid of the game you wish to install
+     read -r c
+     echo input the installation path of the game
+     read -r d
+     mkdir $d
+     ./steamcmd.sh +login $a $b +force_install_dir $d +app_update $c validate 
+else
+       echo input the password of the username you entered
+       read -r b
+       echo input the appid of the game you wish to install
+       read -r c
+       echo input the installation path of the game
+       read -r d
+       mkdir $d
+       ./steamcmd.sh +login $a $b +force_install_dir $d +app_update $c validate
+
+fi 
