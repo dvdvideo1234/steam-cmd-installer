69a4e163c3f77cf3b6a9ff96cb375737a897209e
Gousaid67 <erik.lg@student.intsch.se>
2016-02-15 17:59:35 +0100

    added anonymous logging support

--- steamcmdlazyinstaller2.0.sh
+++ steamcmdlazyinstaller2.0.sh
@@ -19,7 +19,7 @@ then
      echo input the installation path of the game
      read -r d
      mkdir $d
-     ./steamcmd.sh +login $a $b +force_install_dir $d +app_update $c validate 
+     ./steamcmd.sh +login $a +force_install_dir $d +app_update $c validate 
 else
        echo input the password of the username you entered
        read -r b
